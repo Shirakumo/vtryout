c22b4d12e8902dd08ffca1ba5abaa1291192bae6
Yukari Hafner <shinmera@tymoon.eu>
2025-01-11 13:25:50 +0100

    Use the mouth-open layer for animating the mouth.

--- actor.lisp
+++ actor.lisp
@@ -6,18 +6,20 @@
 (defmethod shared-initialize :after ((actor actor) slots &key layers)
   (dolist (layer layers)
     (destructuring-bind (name &optional (strength 1.0)) (enlist layer)
-      (if (animation-layer name actor)
-          (setf (animation-layer name actor) strength)
-          (add-animation-layer name actor :strength strength)))))
+      (add-animation-layer name actor :strength strength :if-exists :supersede))))
 
 ;; KLUDGE
 (defmethod enter :after ((entity animated-entity) (controller animation-controller))
   (setf (animation-controller entity) controller))
 
-(defmethod fk-update ((actor actor) pose tt dt fc)
-  (let* ((bone (node :DEF-JAW (skeleton actor)))
-         (joint (elt pose bone)))
-    #++(!q* (trotation joint) (qfrom-angle +vx+ (deg->rad 45)) (trotation joint))))
+(defmethod update :before ((actor actor) tt dt fc)
+  (let* ((layer (animation-layer :mouth-open actor :if-does-not-exist :create))
+         (seg (harmony:segment 'speech-detection T))
+         (new (if (speech-p seg)
+                  (/ (- (clamp 300.0 (vy (main-frequency seg)) 800.0) 300.0)
+                     500.0)
+                  0.0)))
+    (setf (strength layer) (lpf 0.99 (strength layer) new))))
 
 (defun actor (&optional name)
   (do-scene-graph (node (scene +main+))
@@ -26,8 +28,6 @@
       (return node))))
 
 (defun toggle-layer (name &key (actor (actor)) (strength 1.0))
-  (unless (clip actor)
-    (play :idle actor))
   (if (animation-layer name actor)
       (remove-animation-layer name actor)
       (add-animation-layer name actor :strength strength)))
